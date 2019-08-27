3a7f39fe611261ecab96daed25588042fca655b0
Sean Brogan <sean.brogan@microsoft.com>
2019-08-26 21:03:13 -0700

    Combine WinHostBasedVcVars and HostBasedUnitTestRunner Plugin since they are both only used on Host Based Unit Test builds on Windows.

--- BaseTools/Plugin/HostBasedUnitTestRunner/HostBasedUnitTestRunner.py
+++ BaseTools/Plugin/HostBasedUnitTestRunner/HostBasedUnitTestRunner.py
@@ -16,8 +16,11 @@ import xml.etree.ElementTree
 class HostBasedUnitTestRunner(IUefiBuildPlugin):
 
     def do_post_build(self, thebuilder):
-        shell_env = shell_environment.GetEnvironment()
+        ci_type = thebuilder.env.GetValue('CI_BUILD_TYPE')
+        if ci_type != 'host_unit_test':
+            return 0
 
+        shell_env = shell_environment.GetEnvironment()
         logging.log(edk2_logging.get_section_level(), "Run Host based Unit Tests")
         path = thebuilder.env.GetValue("BUILD_OUTPUT_BASE")
         for arch in thebuilder.env.GetValue("TARGET_ARCH").split():
@@ -54,4 +57,21 @@ class HostBasedUnitTestRunner(IUefiBuildPlugin):
         return 0
 
     def do_pre_build(self, thebuilder):
-        return 0
+        ci_type = thebuilder.env.GetValue('CI_BUILD_TYPE')
+        if ci_type != 'host_unit_test':
+            return 0
+
+        shell_env = shell_environment.GetEnvironment()
+        # Use the tools lib to determine the correct values for the vars that interest us.
+        interesting_keys = ["ExtensionSdkDir", "INCLUDE", "LIB", "LIBPATH", "UniversalCRTSdkDir",
+                            "UCRTVersion", "WindowsLibPath", "WindowsSdkBinPath", "WindowsSdkDir", "WindowsSdkVerBinPath",
+                            "WindowsSDKVersion","VCToolsInstallDir"]
+        vs_vars = locate_tools.QueryVcVariables(interesting_keys, "amd64")
+        for (k,v) in vs_vars.items():
+            if k.upper() == "PATH":
+                shell_env.append_path(v)
+            else:
+                shell_env.set_shell_var(k, v)
+
+        # Set up the reporting type for Cmocka.
+        shell_env.set_shell_var('CMOCKA_MESSAGE_OUTPUT', 'xml')
