b35873333da50aaf1c738844a66e8d067857bb83
Sean Brogan <spbrogan@live.com>
2019-08-28 19:52:37 -0700

    Clean up plugins (#1)
    * Add GuidCheck Plugin.  This requires new pytool-library release
    
    * Add LibraryClass Check plugin to confirm library classes are properly declared.
    
    * Combine WinHostBasedVcVars and HostBasedUnitTestRunner Plugin since they are both only used on Host Based Unit Test builds on Windows.
    
    * Move CI plugins into Ci folder
    
    * Finish GuidCheck and add support for Ignore lists
    
    * Add GuidCheck config to MdeModulePkg.mu.yaml as example
    
    * Fix broken HostBasedUnitTestRunner.  Broken due to bad merge when combining the two plugins.
    
    * Add support for IgnoreHeaderFile config option
    
    * Add empty LibraryClassCheck config
    
    * Update plugins
    
    * UefiCpuPkg updates
    
    * Update HostLibs and TestCode to be HOST_APPLICATION type
    
    * Update MdePkg
    
    * Update test name for GuidCheck
    
    * Update MdePkg and FatPkg
    
    * Update MdeModulePkg yaml
    
    * Update SecurityPkg and PcAtChipsetPkg
    
    * Update yaml for crypto, fmp, and network pkg
    
    * Enable DependencyCheck plugin
    
    * Update Acceptabledependencies for securityPkg
    
    * Remove project_mu scope and use corebuild instead
    
    * Change DscCompleteCheck to ignore INFs of type Host_Application.  This was agreed upon with Intel during meeting.
    
    * Add DscCompleteCheck for Host Unit Tests so that we can be sure all Tests are in the DSC.
    
    * Add BaseBinSecurityLig to FatPkg Dsc
    
    * Add Config for HostBasedUnitTest DscCheck

--- BaseTools/Plugin/HostBasedUnitTestRunner/HostBasedUnitTestRunner.py
+++ BaseTools/Plugin/HostBasedUnitTestRunner/HostBasedUnitTestRunner.py
@@ -6,18 +6,23 @@
 ##
 import os
 import logging
-from edk2toolext import edk2_logging
-from edk2toolext.environment.plugintypes.uefi_build_plugin import IUefiBuildPlugin
-from edk2toolext.environment import shell_environment
-from edk2toollib.utility_functions import RunCmd
 import glob
 import xml.etree.ElementTree
+from edk2toolext.environment.plugintypes.uefi_build_plugin import IUefiBuildPlugin
+from edk2toolext import edk2_logging
+import edk2toollib.windows.locate_tools as locate_tools
+from edk2toolext.environment import shell_environment
+from edk2toollib.utility_functions import RunCmd
+
 
 class HostBasedUnitTestRunner(IUefiBuildPlugin):
 
     def do_post_build(self, thebuilder):
-        shell_env = shell_environment.GetEnvironment()
+        ci_type = thebuilder.env.GetValue('CI_BUILD_TYPE')
+        if ci_type != 'host_unit_test':
+            return 0
 
+        shell_env = shell_environment.GetEnvironment()
         logging.log(edk2_logging.get_section_level(), "Run Host based Unit Tests")
         path = thebuilder.env.GetValue("BUILD_OUTPUT_BASE")
         for arch in thebuilder.env.GetValue("TARGET_ARCH").split():
@@ -54,4 +59,22 @@ class HostBasedUnitTestRunner(IUefiBuildPlugin):
         return 0
 
     def do_pre_build(self, thebuilder):
+        ci_type = thebuilder.env.GetValue('CI_BUILD_TYPE')
+        if ci_type != 'host_unit_test':
+            return 0
+
+        shell_env = shell_environment.GetEnvironment()
+        # Use the tools lib to determine the correct values for the vars that interest us.
+        interesting_keys = ["ExtensionSdkDir", "INCLUDE", "LIB", "LIBPATH", "UniversalCRTSdkDir",
+                            "UCRTVersion", "WindowsLibPath", "WindowsSdkBinPath", "WindowsSdkDir", "WindowsSdkVerBinPath",
+                            "WindowsSDKVersion","VCToolsInstallDir"]
+        vs_vars = locate_tools.QueryVcVariables(interesting_keys, "amd64")
+        for (k,v) in vs_vars.items():
+            if k.upper() == "PATH":
+                shell_env.append_path(v)
+            else:
+                shell_env.set_shell_var(k, v)
+
+        # Set up the reporting type for Cmocka.
+        shell_env.set_shell_var('CMOCKA_MESSAGE_OUTPUT', 'xml')
         return 0
